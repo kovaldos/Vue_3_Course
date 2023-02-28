bdea12860b8209f77e6d5d7bfeac46fb1d604012
kovaldos <ya.kovalev@ya-kovalev.ru>
2023-03-01 01:57:03 +0300

    feat: complete module3 and practice

--- Module3/app.js
+++ Module3/app.js
@@ -1,3 +1,40 @@
-const app = Vue.createApp({})
+const h = Vue.h;
+
+const app = Vue.createApp({
+    data() {
+        return {
+            title: "Это из свойства template",
+        }
+    },
+    methods: {
+        changeTitle() {
+            this.title = "Заголовок изменен!"
+        }
+    },
+    // template: `
+    //     <div class="card center">
+    //         <h1>{{ title }}</h1>
+    //         <button
+    //             class="btn primary"
+    //             @click="title = 'Заголовок изменен!'"
+    //         >
+    //             Изменить заголовок
+    //         </button>
+    //     </div>
+    // `,
+    render() {
+        return h("div",
+            {
+                className: "card center"
+            }, [
+                h("h1", {
+                    className: "title title--h1",
+                }, this.title),
+                h("button", {className: "btn primary", onClick: this.changeTitle},
+                    "Изменить заголовок")
+            ]
+        )
+    }
+})
 
 app.mount("#app")
