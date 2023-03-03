146ee64e7dfd75e0b9fc3f7aa581e1994bee9fb1
kovaldos <ya.kovalev@ya-kovalev.ru>
2023-03-03 10:51:52 +0300

    refactor: custom settings

--- Module4/src/App.vue
+++ Module4/src/App.vue
@@ -1,71 +1,59 @@
-<script setup>
-
-</script>
-
 <template>
-  <h1>Hello Vue 3!</h1>
+  <v-header></v-header>
+  <div class="container pt-1">
+    <div class="card">
+      <h1>Latest news <small>{{ now }}</small></h1>
+      <span>viewed: {{ openRate }}</span> | <span>read: {{ readRate }}</span>
+    </div>
+    <v-news v-for="item in news"
+            :key="item.id"
+            :id="item.id"
+            :title="item.title"
+            :is-open="item.isOpen"
+            :was-read="item.wasRead"
+            @open-news="openNews"
+            @mark-read="readNews"
+            @mark-unread="unreadNews"
+    >
+    </v-news>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+import VNews from "@/components/VNews.vue";
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+export default {
+  components: {VNews},
+  data() {
+    return {
+      now: new Date().toLocaleDateString(),
+      news: [
+        {id: 1, title: "Vue 3 успешно работает!", isOpen: false, wasRead: false},
+        {id: 2, title: "Vue 3 Composition API набирает обороты", isOpen: false, wasRead: false},
+      ],
+      openRate: 0,
+      readRate: 0
+    }
+  },
+  methods: {
+    openNews() {
+      this.openRate++
+    },
+    readNews(id) {
+      const idx = this.news.findIndex((news) => news.id === id);
+      this.news[idx].wasRead = true;
+      this.readRate++
+    },
+    unreadNews(id) {
+      const news = this.news.find((news) => news.id === id);
+      news.wasRead = false;
+      this.readRate--
+    }
   }
 }
+
+</script>
+<style scoped lang="scss">
+
 </style>
--- Module4/src/main.js
+++ Module4/src/main.js
@@ -1,10 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import uiComponents from '@/components/UI'
 
 import './assets/main.css'
+import VHeader from "@/components/VHeader.vue";
+
+
 
 const app = createApp(App)
+
+app.component('v-header', VHeader);
+uiComponents.forEach((uiComponent) => {
+    app.component(uiComponent.name, uiComponent)
+})
+
 
 app.use(router)
 
