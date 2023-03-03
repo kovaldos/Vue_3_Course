02ba01f56aa438a6308dd64b2f30bf561c645e7a
kovaldos <ya.kovalev@ya-kovalev.ru>
2023-03-03 17:12:54 +0300

    feat: add provide and inject

---
+++ Module4/src/components/VNewsList.vue
@@ -0,0 +1,23 @@
+<template>
+  <hr>
+  <ul class="news-list">
+    <li class="news-list__item" v-for="item in news" :class="{'read': item.wasRead}">{{ item.title }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "v-news-list",
+  inject: ["news"],
+}
+</script>
+
+<style scoped lang="scss">
+  .news-list {
+    &__item {
+      &.read {
+        opacity: 0.6;
+      }
+    }
+  }
+</style>
