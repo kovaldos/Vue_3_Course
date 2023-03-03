be1a2004ca43aa37ab6f38d4e1195a4832ddaac6
kovaldos <ya.kovalev@ya-kovalev.ru>
2023-03-03 10:49:03 +0300

    feat: add header component

---
+++ Module4/src/components/VHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header__logo" v-if="$route.path === '/'">
+        <span>
+          <img src="@/assets/logo.svg" alt="App logo"/>
+        </span>
+      </div>
+      <div class="header__logo" v-else>
+        <router-link to="/">
+          <img src="@/assets/logo.svg" alt="App logo"/>
+        </router-link>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  width: 100%;
+  height: 5rem;
+  background-color: #fff;
+  padding: 1rem 0px;
+  &__logo {
+    max-width: 3rem;
+    max-height: 3rem;
+  }
+}
+</style>
