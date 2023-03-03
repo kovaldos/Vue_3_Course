55fd8554f44fe0db763b6a603b2c02ed34cc2d3b
kovaldos <ya.kovalev@ya-kovalev.ru>
2023-03-03 17:10:40 +0300

    refactor: edit main.js

--- Module4/src/main.js
+++ Module4/src/main.js
@@ -2,11 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import uiComponents from '@/components/UI'
-
 import './assets/main.css'
 import VHeader from "@/components/VHeader.vue";
-
-
 
 const app = createApp(App)
 
@@ -14,7 +11,6 @@ app.component('v-header', VHeader);
 uiComponents.forEach((uiComponent) => {
     app.component(uiComponent.name, uiComponent)
 })
-
 
 app.use(router)
 
