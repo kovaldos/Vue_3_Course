a1b1bb7aa395c7dc43c1edf1a79c2260df71bead
kovaldos <ya.kovalev@ya-kovalev.ru>
2023-03-10 21:53:53 +0300

    init: start module

---
+++ Module7/src/components/AppAlert.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="alert" :class="type">
+    <h3 class="alert-title">{{ title }}</h3>
+    <p class="alert-text">{{ text }}</p>
+    <custom-btn
+        v-if="closable"
+        :class="type"
+        @click="$emit('close')"
+    >
+      Close
+    </custom-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppAlert",
+  emits: ["close"],
+  props: {
+    title: String,
+    text: String,
+    closable: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    type: {
+      type: String,
+      required: false,
+      default: 'primary',
+      validator(val) {
+        return ["primary", "danger", "warning"].includes(val)
+      }
+    },
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Module7/src/components/UI/CustomBtn.vue
@@ -0,0 +1,38 @@
+<template>
+  <button
+      class="btn"
+      :type="nativeType"
+      :class="cssClass"
+      :disabled="disabled"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "custom-btn",
+  props: {
+    nativeType: {
+      type: String,
+      required: false,
+      default: "button"
+    },
+    cssClass: {
+      type: String,
+      required: false,
+      default: ""
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "@/scss/style.scss";
+@import "@/scss/common/btn.scss";
+</style>
